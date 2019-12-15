5d02fee57832d4df4e9ee2c95c90eed8c9c46c86
Lee-Ann <leeann74c@gmail.com>
2019-12-16 00:25:52 +0200

    Chapter 1 exercises added

---
+++ Chapter-1/exercise one.one.rb
@@ -0,0 +1,3 @@
+first_name = "Lee-Ann "
+last_name = "Clark"
+puts first_name  + last_name
---
+++ Chapter-1/exercise one.seven.rb
@@ -0,0 +1,7 @@
+SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+  from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+  
+  This means that there is an error.
+  There is a round bracket ')' when a } was expected so go back an look 
+  where you used a bracket to close and it might be either highlighted or underlined
+  
