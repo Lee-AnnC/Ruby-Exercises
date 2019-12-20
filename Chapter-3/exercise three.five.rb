b79cc802d778c6282a0955f0e8f4297a57a1bfa1
Lee-Ann <leeann74c@gmail.com>
2019-12-20 22:30:00 +0200

    Chapter 3 exercises

---
+++ Chapter-3/exercise three.five.rb
@@ -0,0 +1,7 @@
+def scream(words)
+  words = words + "!!!!"
+end
+
+puts scream("Yippeee")
+
+2. return nil
---
+++ Chapter-3/exercise three.six.rb
@@ -0,0 +1,8 @@
+ArgumentError: wrong number of arguments (1 for 2)
+  from (irb):1:in `calculate_product'
+  from (irb):4
+  from /Users/username/.rvm/rubies/ruby-2.0.0-p353/bin/irb:12:in `<main>'
+  
+  you are calling a method called calculate_product
+  you should have two arguments
+  only one is supplied
